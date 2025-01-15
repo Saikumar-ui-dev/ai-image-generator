df15a7d788f1ca9587c92f13e7b4d903314730a4
Saikumar-ui-dev <saikumarparisa13@gmail.com>
2025-01-15 12:06:52 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import {registerUser, loginUser, userCredits} from '../controllers/userController.js'
+import {registerUser, loginUser, userCredits, getUsers} from '../controllers/userController.js'
 import userAuth from '../middlewares/auth.js'
 
 const router = express.Router()
 
 router.post('/register',registerUser)
 router.post("/login", loginUser);
+router.get("/", getUsers);
 router.get("/credits",userAuth, userCredits);
 // router.post("/pay", userAuth, paymentRazorPay);
 
